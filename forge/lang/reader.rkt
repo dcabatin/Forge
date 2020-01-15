39c8c04b73682104e53f5c49fc6bbcab99f7db00
iambadatnames <charles_cutting@brown.edu>
2020-01-15 15:45:36 -0500

    Update reader.rkt

--- forge/lang/reader.rkt
+++ forge/lang/reader.rkt
@@ -21,17 +21,30 @@
      `,(node/int/constant datum)]
     [else datum]))
 
+; find all the sig declarations and lift their binding to the top level.
+(define (is-sig-decl datum) (equal? (car datum) 'SigDecl)) 
+
+(define (pull-sigs datum)
+  (map (lambda (x) `(pre-declare-sig ,@(cdr (cdr x)))) (filter is-sig-decl datum)))
+
 
 (define (read-syntax path port)
   ; (define src-datum (port->list read port))
   (define parse-tree (parse path (make-tokenizer port)))
+
+  
   (define src-datum (cdr (syntax->datum parse-tree)))
   ; (println src-datum)
   ; don't use format-datums, because it's awful with quotes.
   (define transformed (replace-ints src-datum))
 
+  (define sig-inits (pull-sigs transformed))
+
+  (define final `(,@(car transformed) ,@(append sig-inits (cdr transformed))))
+
   (define module-datum `(module kkcli ,forge-path
-                          ,@transformed))
+                          ,@final))
+  
   (define stx (datum->syntax #f module-datum))
   ; (browse-syntax stx)
   stx)
